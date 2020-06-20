a1d9b7c5561b2e0d63cf37dbdad8718a5c09ba30
Dave Schinkel <dschinkel@gmail.com>
2020-06-19 21:07:48 -0500

    Displays a keypad with numbers

--- src/UI/CalculatorUI.js
+++ src/UI/CalculatorUI.js
@@ -4,7 +4,28 @@ import Calculator from "../Core/Calculator";
 const CalculatorUI = () => {
 	const calculator = new Calculator();
 	calculator.evaluate();
-	return <div data-testid='display'>{calculator.getResult()}</div>;
+	return (
+		<>
+			<div data-testid='display'>{calculator.getResult()}</div>
+			<div>
+				<NumberKeys data-testid="keypad"/>
+			</div>
+		</>
+
+	);
+
+}
+
+export const NumberKeys = () => {
+	return (
+		<>
+			{
+				[0,1,2,3,4,5,6,7,8,9].map(number => {
+					return <div data-testid='numberKey'>{number}</div>
+				})
+			}
+	</>
+)
 }
 
 export default CalculatorUI;
--- test/calculator.ui.spec.js
+++ test/calculator.ui.spec.js
@@ -1,7 +1,8 @@
 import React from "react";
-const Enzyme = require('enzyme');
 import Adapter from 'enzyme-adapter-react-16';
-import CalculatorUI from "../src/UI/CalculatorUI";
+import CalculatorUI, {NumberKeys} from "../src/UI/CalculatorUI";
+
+const Enzyme = require('enzyme');
 Enzyme.configure({ adapter: new Adapter() });
 
 const test = require('ava');
@@ -10,5 +11,15 @@ const {shallow} = require("enzyme");
 test('display defaults to zero', t => {
 	const display = shallow(<CalculatorUI />).find('[data-testid="display"]');
 	t.is(display.text(), '0');
+});
+
+test('displays a keypad', t => {
+	const keypad = shallow(<CalculatorUI />).find('[data-testid="keypad"]');
+  t.is(keypad.length,1);
+});
+
+test('displays numbers', t => {
+  const numberKeys = shallow(<NumberKeys />).find('[data-testid="numberKey"]');
+  t.is(numberKeys.length,10);
 });
 
